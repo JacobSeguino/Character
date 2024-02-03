5147b43078342549961d8052404bb7416274154e
JacobSeguino <jacob.seguino@gmail.com>
2024-02-02 18:29:01 -0600

    added an option to choce one name or a "full name" and added a yaml with a list of lastName.

--- scripts/namePicker.py
+++ scripts/namePicker.py
@@ -1,14 +1,25 @@
 import random
 import yaml
+import sys
 
 nameList = []
 resourcesPath = "scripts/resources/"
+firstNameYml = "firstNames.yml"
+lastNameYml = "lastNames.yml"
+race = 'human'
 
-def randomName(ymalName):
+def randomName(ymalName, raceChoice):
     with open(resourcesPath + ymalName, 'r') as stream:
-        nameList = yaml.safe_load(stream)['human']
+        nameList = yaml.safe_load(stream)[raceChoice]
 
-    print(nameList[random.randrange(len(nameList))-1])
+    sys.stdout.write(nameList[random.randrange(len(nameList))-1]+" ")
+
+def namePicker(choice):
+    randomName(firstNameYml, race)
+    if choice == "full" or choice == "full name":
+        randomName(lastNameYml, race)
 
 if __name__ == '__main__':
-    randomName("firstNames.yml")
+    print('full name or single')
+    x = input()
+    namePicker(x)
